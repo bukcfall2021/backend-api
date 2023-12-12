f8132d1650747419f812c28c6331501ffed28fd9
ArURehman <areebcodes1991@gmail.com>
2023-12-12 17:18:00 +0500

    Edited createController to have new Models

--- src/auth/controllers/userControllers/userAuthController.js
+++ src/auth/controllers/userControllers/userAuthController.js
@@ -3,29 +3,6 @@ const createController = require("../../../global/utils/createController");
 // /api/auth/user/login
 module.exports.login = createController(async (req, res) => {
 
-  const User = req.app.locals.db.User;
-
-  try {
-    const user = await User.create({
-      name: "Mubashir Ali",
-    });
-    const cart = await Cart.create({
-
-    })
-
-    const allUsersFromDB = await User.findAll();
-
-    res.send({
-      success: true,
-      data: allUsersFromDB,
-      error: null,
-    });
-  } catch (e) {
-    res.send({
-      success: false,
-      data: null,
-      error: e.message,
-    });
-  }
+    const User = req.app.locals.db.User
 
 });
