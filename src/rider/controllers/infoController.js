71c1fbe785c7ce83f1a4bfa9dff60edff16f0699
ArURehman <areebcodes1991@gmail.com>
2023-12-16 20:27:56 +0500

    Added password change controller

--- src/rider/controllers/infoController.js
+++ src/rider/controllers/infoController.js
@@ -1,12 +1,13 @@
 const createController = require('../../global/utils/createController');
+const bcrypt = require('bcrypt');
 
 module.exports.edit = createController(async (req, res) => {
 
     const Rider = req.app.locals.db.Rider;
     const data = req.body;
 
-    if(!data){
-        return res.status(400).send({error: 'Invalid Input'})
+    if(!data || !data.id){
+        return res.status(400).send({error: 'Invalid Data'})
     }
 
     try {
@@ -38,6 +39,32 @@ module.exports.editPassword = createController(async (req, res) => {
     const Rider = req.app.locals.db.Rider;
     const data = req.body;
 
-    
+    if(!data || !data.oldPassword || !data.id || !data.newPassword){
+        return res.status(400).send({error: "Invalid Data"});
+    }
+
+    try {
+
+        const rider = await Rider.findByPk(data.id);
+        if(!rider){
+            return res.status(400).send({error: "Rider Doesn't Exist"});
+        }
+
+        const isValid = await bcrypt.compare(data.oldPassword, rider.password);
+        if(!isValid){
+            return res.status(400).send({error: "Old Password is Invalid"});
+        }
+
+        const encryptedPassword = await bcrypt.hash(data.newPassword, 10);
+
+        const newRider = await rider.update({password: encryptedPassword});
+        if(!newRider){
+            throw error;
+        }
+        return res.status(200).send({sucess: true, rider: newRider});
+        
+    } catch (error) {
+        return res.status(500).send({error: error});
+    }
 
 });
--- src/rider/routes/infoRoute.js
+++ src/rider/routes/infoRoute.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const infoController = require('../controllers/infoController');
 
 router.put('/edit', infoController.edit);
-router.put('/edit-password')
+router.put('/edit-password', infoController.editPassword)
 
 module.exports = router;
