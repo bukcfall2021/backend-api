cad9b7e2cb73163ede280f968df58a68ec7f5f4f
bukcfall2021 <149307222+bukcfall2021@users.noreply.github.com>
2023-12-18 07:53:10 +0500

    Revert "variant CRUD APIs created, promo CRUD APIs created, variant and promo routes added to vender routes"

--- src/vender/routes/promoRoute.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const promoController = require('../controllers/promoController');
-
-router.get('/get', promoController.get);
-router.post('/create', promoController.create);
-router.delete('/delete', promoController.delete);
-router.put('/update', promoController.update);
-
-module.exports = router;
