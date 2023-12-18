2ee9f15a0a5ce3e317c1d012e4c5a3a93e7fadf7
ArURehman <areebcodes1991@gmail.com>
2023-12-18 07:48:05 +0500

    Fixed Issues in Promo and Variant Controllers

--- src/vender/controllers/promoController.js
+++ src/vender/controllers/promoController.js
@@ -3,19 +3,19 @@ const promoController = require('../../global/utils/createController');
 // api/vendor/promo/get
 module.exports.get = promoController(async (req, res) => {
     const Promo = req.app.locals.db.Promo;
-    const data = req.query;
+    const id = req.params.id;
 
-    if (!data || !data.id) {
+    if (!id) {
         return res.status(400).send({error: "Invalid Input"});
     }
 
     try {
-        const promo = await Promo.findOne({where: {id: data.id}});
+        const promo = await Promo.findOne({where: {id: id}});
         if (!promo) {
             return res.status(404).send({error: "Promo Not Found"});
         }
-
         return res.status(200).send({promo: promo});
+        
     } catch (err) {
         return res.status(500).send({error: err});
     }
@@ -25,21 +25,21 @@ module.exports.get = promoController(async (req, res) => {
 module.exports.create = promoController(async (req, res) => {
 
     const Promo = req.app.locals.db.Promo;
-    const Item = req.app.locals.db.Item;
     const data = req.body;
 
     if(!data || !data.discountPercentage || !data.expiryDate || !data.code){
         return res.status(400).send({error: "Invalid Input"});
     }
 
-    const itemFind = await Item.findOne({where: {id: data.itemId}});
-    if(!itemFind){
-        return res.status(404).send({error: "Item Not Found"});
-    }
-
     try {
-        const promo = await Promo.create(data);
+
+        const promo = await Promo.create({
+            expiryDate: data.expiryDate,
+            discountPercentage: data.discountPercentage,
+            code: data.code
+        });
         return res.status(200).send({promo: promo});
+
     } catch (err) {
         return res.status(500).send({error: err});
     }
@@ -47,33 +47,6 @@ module.exports.create = promoController(async (req, res) => {
 
 // api/vendor/promo/update
 module.exports.update = promoController(async (req, res) => {
-
-    const Promo = req.app.locals.db.Promo;
-    const data = req.body;
-
-    if(!data || !data.discountPercentage || !data.expiryDate || !data.code){
-        return res.status(400).send({error: "Invalid Input"});
-    }
-
-    try {
-        const promo = await Promo.findOne({where: {id: data.id}});
-        if(!promo){
-            return res.status(404).send({error: "Promo Not Found"});
-        }
-
-        promo.discountPercentage = data.discountPercentage;
-        promo.expiryDate = data.expiryDate;
-        promo.code = data.code;
-        await promo.save();
-
-        return res.status(200).send({promo: promo});
-    } catch (err) {
-        return res.status(500).send({error: err});
-    }
-});
-
-// api/vendor/promo/delete
-module.exports.delete = promoController(async (req, res) => {
 
     const Promo = req.app.locals.db.Promo;
     const data = req.body;
@@ -88,11 +61,40 @@ module.exports.delete = promoController(async (req, res) => {
             return res.status(404).send({error: "Promo Not Found"});
         }
 
-        const promoToReturn = {...promo.dataValues}; // Copy the promo data before destroying it
+        const updatedPromo = await promo.update({
+            expiryDate: data?.expiryDate,
+            discountPercentage: data?.discountPercentage,
+            code: data?.code
+        })
+        return res.status(200).send({promo: updatedPromo});
 
-        await promo.destroy();
+    } catch (err) {
+        return res.status(500).send({error: err});
+    }
+});
 
-        return res.status(200).send({promo: promoToReturn});
+// api/vendor/promo/delete
+module.exports.delete = promoController(async (req, res) => {
+
+    const Promo = req.app.locals.db.Promo;
+    const id = req.params.id;
+
+    if(!id){
+        return res.status(400).send({error: "Invalid Input"});
+    }
+
+    try {
+        const promo = await Promo.findOne({where: {id: id}});
+        if(!promo){
+            return res.status(404).send({error: "Promo Not Found"});
+        }
+
+        const promoDestroy = await promo.destroy();
+        if(!promoDestroy){
+            throw error;
+        }
+        return res.status(200).send({success: true, promo: promoDestroy});
+
     } catch (err) {
         return res.status(500).send({error: err});
     }
